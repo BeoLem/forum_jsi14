e30449fa2f649af32505d6cb7090162f43d30b47
BeoLem <lamhanoi2010@gmail.com>
2022-12-24 15:15:56 +0700

    Blog creator page

--- client/controllers/Blog.ts
+++ client/controllers/Blog.ts
@@ -4,9 +4,9 @@ import { Response } from '../typings/Response'
 import { CreateRespond } from '../utils/Response'
 
 export const GetCreateBlogPage = async (req: Request, res: Response) => {
-  // if (res.locals.user?.id) return res.redirect('/auth/logout')
   res.render("blogs/create", {
     query: req.query,
+    cookies: req.cookies,
     locals: res.locals || {},
     page: {
       title: "CFrum | Create a blog",
