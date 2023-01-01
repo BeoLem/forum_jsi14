30a16d74891678a74868f737e8bec204cc896ef1
BeoLem <lamhanoi2010@gmail.com>
2023-01-01 13:32:14 +0700

    Fix some bugs

--- client/app.ts
+++ client/app.ts
@@ -1,56 +1,55 @@
 // Import modules
-import express from 'express'
-import path from 'path'
-import cookieParser from 'cookie-parser'
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
 
-import logger from './utils/Logger'
+import logger from "./utils/Logger";
 
 // Import routes
-import UserRouter from './routes/User'
-import AuthRouter from './routes/Auth'
-import BlogRouter from './routes/Blog'
-import RootRouter from './routes/Root'
+import UserRouter from "./routes/User";
+import AuthRouter from "./routes/Auth";
+import BlogRouter from "./routes/Blog";
+import RootRouter from "./routes/Root";
 
 // Initialize the project
-const app = express()
+const app = express();
 
 // Middlewares
-app.use(express.json())
+app.use(express.json());
 app.use(
-    express.urlencoded({
-        extended: false,
-    })
-)
+  express.urlencoded({
+    extended: false,
+  })
+);
 app.use((req, res, next) => {
-    next()
-    console.log(req)
-    logger.info(
-        `[${req.method.toUpperCase()}] [${req.socket.remoteAddress}] ${req.baseUrl}`
-    )
-})
-app.use(cookieParser())
+  next();
+  logger.info(
+    `[${req.method.toUpperCase()}] [${req.socket.remoteAddress}] ${req.url}`
+  );
+});
+app.use(cookieParser());
 
 // Routes
-app.use('/users', UserRouter)
-app.use('/auth', AuthRouter)
-app.use('/blogs', BlogRouter)
-app.use('/', RootRouter)
-app.use('/assets', express.static(path.join(__dirname, 'assets')))
+app.use("/users", UserRouter);
+app.use("/auth", AuthRouter);
+app.use("/blogs", BlogRouter);
+app.use("/", RootRouter);
+app.use("/assets", express.static(path.join(__dirname, "assets")));
 
-app.use(function(req, res, next) {
-    res.render("web/error.ejs", {
-      page: {
-        status: 404,
-        message: "You're lost",
-        title: "CFrum | Not Found",
-        color: "red",
-        redirect: "/"
-      },
-    });
-})
+app.use(function (req, res, next) {
+  res.render("web/error.ejs", {
+    page: {
+      status: 404,
+      message: "You're lost",
+      title: "CFrum | Not Found",
+      color: "red",
+      redirect: "/",
+    },
+  });
+});
 
 // Application Configurations
-app.set('view engine', 'ejs')
-app.set('views', path.join(__dirname, 'views'))
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
-export { app }
+export { app };
